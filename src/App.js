44f63ceec71ac5bf93d8f25b460a995fb45e9b3b
Al-Junaed Islam <brohi96@gmail.com>
2021-06-10 20:26:49 +0600

    navbar done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return <div>
+            <div className="bg-white h-screen flex flex-col justify-center items-center">
+                <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14">
+                    React-Tailwind
+                </h1>
+            </div>
+        </div>;
+};
+
+export default Header;
